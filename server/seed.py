6695d4d1ea0e207a20b4585ac568450231ff6740
Uwes Ahabab <uwesahabab@gmail.com>
2024-06-28 00:19:25 +0300

    Commits

--- server/seed.py
+++ server/seed.py
@@ -10,7 +10,7 @@ with app.app_context():
     Earthquake.query.delete()
 
     # Add several Earthquake instances to the "earthquakes" table
-    db.session.add(Earthquake(magnitude=9.5, location="Chile", year=1960))
+    db.session.add(Earthquake(magnitude=9.5, location="Chile", year=1960))  
     db.session.add(Earthquake(magnitude=9.2, location="Alaska", year=1964))
     db.session.add(Earthquake(magnitude=8.6, location="Alaska", year=1946))
     db.session.add(Earthquake(magnitude=8.5, location="Banda Sea", year=1934))
